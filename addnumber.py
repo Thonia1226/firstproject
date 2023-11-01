b7c4194fa17adc58020ecd824efc9ba5594af39c
Thonia1226 <97959510+Thonia1226@users.noreply.github.com>
2023-11-01 16:02:51 +0100

    Adding and subtrac ting with print alert in python

---
+++ addnumber.py
@@ -0,0 +1,36 @@
+#this code shows how to add numbers  and subtract numbers and gives print alert of the resultst
+
+
+# this is to write a program that will add two numbers together
+a=14
+b=8
+z=a+b
+y=a-b
+print("This is the result of the addition " + str(z))
+if z==19:
+    print("Hurray! i got it")
+else:
+     print("oh sorry! you are wrong")    
+
+# to subract
+
+
+print("This is the result of the subtraction " + str(y))
+if y>5:
+    print("oh you are correct")
+else:
+     print("oh try again")    
+
+
+#cocde to multiply
+
+
+print("This is the result of the multiplication " + str(a*b))
+
+
+#cocde to division
+
+
+print("This is the result of the division " + str(a/b))
+
+
