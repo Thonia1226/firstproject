3bb618ccd1df2b72a1f65b2cb6c65bd669be18ad
Thonia1226 <97959510+Thonia1226@users.noreply.github.com>
2023-11-01 15:51:38 +0100

    Update feyi.py

--- feyi.py
+++ feyi.py
@@ -1,1 +1,4 @@
-beginner at python
+#This is a python code where the system ask the user her name and the system print(hello yourname)
+print("what is your name")
+x = input("your name please")
+print("Hello, " + x)
