ebdeb9eb1f8606f06b5a63c572e96b28408eac34
Thonia1226 <97959510+Thonia1226@users.noreply.github.com>
2023-11-01 15:55:10 +0100

    Create guess3.py
    python code for guessing numbers added

---
+++ guess3.py
@@ -0,0 +1,23 @@
+## A python code for gueesing numbers
+
+import random
+
+number = random.randint(1, 25)
+
+number_of_guesses = 0
+
+while number_of_guesses < 5:
+    print('Guess a number between 1 and 25:')
+    guess = input()
+    guess = int(guess)
+
+    number_of_guesses = number_of_guesses + 1
+
+    if guess == number:
+        break
+
+if guess == number:
+    print('You guessed the number in ' + str(number_of_guesses) + ' tries!')
+
+else:
+    print('You did not guess the number. The number was ' + str(number))
